1756f0508b225c00ece6c71b1912ffdc55d258ae
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-03 10:09:09 -0300

    feat(server.ts): add create-event route registration to the fastify app for handling event creation requests

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createEvent } from './routes/create-event'
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
