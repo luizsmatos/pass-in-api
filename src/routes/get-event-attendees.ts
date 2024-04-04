6bc7ae45d2d0255a02cfa511df5f28fac8789e8b
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-04 15:33:55 -0300

    feat(get-event-attendees.ts): add route to retrieve event attendees with pagination and search

---
+++ src/routes/get-event-attendees.ts
@@ -0,0 +1,72 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function getEventAttendees(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/events/:eventId/attendees',
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        querystring: z.object({
+          query: z.string().nullish(),
+          page: z.string().nullish().default('1').transform(Number),
+        }),
+        response: {
+          200: z.object({
+            attendees: z.array(
+              z.object({
+                id: z.number(),
+                name: z.string(),
+                email: z.string(),
+                createdAt: z.date(),
+                checkedInAt: z.date().nullable(),
+              }),
+            ),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params
+      const { page, query } = request.query
+
+      const attendees = await prisma.attendee.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          checkIn: {
+            select: {
+              createdAt: true,
+            },
+          },
+        },
+        where: query
+          ? {
+              eventId,
+              name: { contains: query },
+            }
+          : { eventId },
+        take: 10,
+        skip: (page - 1) * 10,
+      })
+
+      return reply.send({
+        attendees: attendees.map((item) => {
+          return {
+            id: item.id,
+            name: item.name,
+            email: item.email,
+            createdAt: item.createdAt,
+            checkedInAt: item.checkIn?.createdAt ?? null,
+          }
+        }),
+      })
+    },
+  )
+}
