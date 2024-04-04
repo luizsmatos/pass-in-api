2710c0cd8033e139f84043f912b557e682c7c4b2
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-04 15:14:41 -0300

    feat(check-in.ts): add new route for attendee check-in functionality

---
+++ src/routes/check-in.ts
@@ -0,0 +1,41 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function checkIn(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/attendees/:attendeeId/check-in',
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int().positive(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { attendeeId } = request.params
+
+      const attendeeCheckIn = await prisma.checkIn.findUnique({
+        where: {
+          id: attendeeId,
+        },
+      })
+
+      if (attendeeCheckIn) {
+        throw new Error('Attendee already checked in!')
+      }
+
+      await prisma.checkIn.create({
+        data: {
+          attendeeId,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
