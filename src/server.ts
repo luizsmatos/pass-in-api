1b57ee4c89e0be41eba00ca9537312be60c302e9
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-04 16:50:31 -0300

    feat(server.ts): add error handling middleware to improve error management in the application

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ import { getEvent } from './routes/get-event'
 import { getAttendeeBadge } from './routes/get-attendee-badge'
 import { checkIn } from './routes/check-in'
 import { getEventAttendees } from './routes/get-event-attendees'
+import { errorHandler } from './error-handler'
 
 const app = fastify()
 
@@ -41,6 +42,8 @@ app.register(getEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
+
+app.setErrorHandler(errorHandler)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
